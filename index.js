82202a2ff7572d22d8bc9d667a8e8e5d1ac8ce75
Alexandr Ivanov <arlert@mail.ru>
2022-07-14 13:03:42 +0300

    add version

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ async function main() {
 
     const major = core.getInput('major');
     const minor = core.getInput('minor');
-    const patch = core.getInput('patch');
-    const revision = core.getInput('revision');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const username = github.context.payload.repository.owner.login;
@@ -17,26 +15,28 @@ async function main() {
     const package_type = core.getInput('package_type');
     const pivot_package_name = core.getInput('pivot_package_name');
 
-    const version = `${major}.${minor}.${patch}.${revision}`;
-    core.setOutput("version", version);
+    let target_version = `${major}.${minor}.0`;
+    try {
+      const result = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
+        package_type: package_type,
+        package_name: pivot_package_name,
+        username: username
+      });
+      let last_version = result.data[0].name;
+      const parts = last_version.split('.');
+      const last_major = parseInt(parts[0]);
+      const last_minor = parseInt(parts[1]);
+      const last_patch = parseInt(parts[2]);
 
-    var result = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
-      package_type: package_type,
-      package_name: pivot_package_name,
-      username: username
-    });
+      const target_patch = major == last_major && minor == last_minor ? last_patch + 1 : 0;
 
-    console.log(result);
-
-
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput('who-to-greet');
-    // console.log(`Hello ${nameToGreet}!`);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+      target_version = `${major}.${minor}.${target_patch}`;
+    }
+    catch (e) {
+      console.warn(e);
+    }
+    
+    core.setOutput("version", target_version);
   } catch (error) {
     core.setFailed(error.message);
   }
