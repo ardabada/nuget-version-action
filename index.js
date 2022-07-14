fed0025bc5302611c7f6652166a482c16ddaf680
Alexandr Ivanov <arlert@mail.ru>
2022-07-14 11:42:31 +0300

    add version

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+  const major = core.getInput('major');
+  const minor = core.getInput('minor');
+  const patch = core.getInput('patch');
+  const revision = core.getInput('revision');
+
+  console.log(`${major}.${minor}.${patch}.${revision}`);
+
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
